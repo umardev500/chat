86dc90b4183430a284258ae04a7e93da7282e6b8
Umar <umar.dev500@gmail.com>
2024-03-04 11:28:05 +0700

    add init migration

--- database/migrations/20240301074141_users.up.sql
+++ database/migrations/20240301074141_users.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
